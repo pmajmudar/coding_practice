7a20bb04f8492c474d85e4eac80786b5e47b9837
Prash <prash@pelucid.com>
2018-09-30 16:57:29 +0100

    add class

--- form_sorter.py
+++ form_sorter.py
@@ -1,3 +1,9 @@
 import docx
 
 path_to_form = '/home/prash/sandpit/pete_k_code/data/demo_forms/Arse.docx'
+
+doc = docx.Document(path_to_form)
+
+class FormProcessor(object):
+    def __init__(selfs, path_to_form):
+        pass
